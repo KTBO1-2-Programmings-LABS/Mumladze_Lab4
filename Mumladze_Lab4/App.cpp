1b2683bb6fb17cbeccc5e229ce5694b5abd318ef
Aleksandr <142475782+AleksandrMum@users.noreply.github.com>
2024-04-01 18:49:44 +0300

    Все переделано и закончено
    Вместо трех классов один, DataManager. Работает со всеми тремя файлами.
    Отчет сделан

--- Mumladze_Lab4/App.cpp
+++ Mumladze_Lab4/App.cpp
@@ -3,11 +3,22 @@
 using namespace System;
 
 int main(array<String^>^ args) {
-	LinkManager^ lm = gcnew LinkManager();
-	BookManager^ bm = gcnew BookManager(lm);
-	SubjectManager^ sm = gcnew SubjectManager(lm);
-	lm->ReadLinkListFromFile();
-	bm->ReadBookListFromFile();
-	sm->ReadSubjectListFromFile();
-	return 0;
+	DataManager^ dm = gcnew DataManager();
+	dm->ReadDataFiles();
+
+	dm->AddNode(T_BOOK, dm->MakeNode(T_BOOK, "012345; Книга; Автор"));
+	dm->AddNode(T_SUBJECT, dm->MakeNode(T_SUBJECT, "111; Физра; Спорт"));
+	dm->AddNode(T_LINK, dm->MakeNode(T_LINK, "012345; 111"));
+
+	Object^ del_book = dm->MakeNode(T_BOOK, "012345; -; -");
+	dm->DeleteNode(T_BOOK, del_book);
+	Object^ del_subj = dm->MakeNode(T_SUBJECT, "111; -; -");
+	dm->DeleteNode(T_SUBJECT, del_subj);
+
+	dm->AddNode(T_BOOK, dm->MakeNode(T_BOOK, "012345; Книга; Автор"));
+	dm->WriteDataFiles();
+
+	for (int i = 0; i < 1; i++) {
+		i--;
+	};
 }
